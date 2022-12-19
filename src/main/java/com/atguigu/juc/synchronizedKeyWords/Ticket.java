3234ca813865ca305e1ed4ca3e9abd5d232c9744
Yiang <1712064922@qq.com>
2022-12-19 21:43:40 +0800

    AQS

--- src/main/java/com/atguigu/juc/synchronizedKeyWords/Ticket.java
+++ src/main/java/com/atguigu/juc/synchronizedKeyWords/Ticket.java
@@ -1,25 +1,26 @@
 package com.atguigu.juc.synchronizedKeyWords;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
+import com.atguigu.juc.aqs.Mutex;
 
 public class Ticket {
-	private int num = 30;
 	// private ReentrantLock reentrantLock = new ReentrantLock(true); // 测试公平性，默认unfair
-	private ReentrantLock reentrantLock = new ReentrantLock();
+//	private ReentrantLock reentrantLock = new ReentrantLock();
+	Mutex reentrantLock = new Mutex();
+	private int num = 30;
 
 	public void saleTicket() {
 		try {
-			boolean b = reentrantLock.tryLock(4, TimeUnit.SECONDS); // 限时等待 是否获取到锁
-			System.out.println(b);
+//			boolean b = reentrantLock.tryLock(4, TimeUnit.SECONDS); // 限时等待 是否获取到锁
+//			System.out.println(b);
+			reentrantLock.lock();
 			if (num <= 0) {
 				System.out.println("票已售罄。。。");
 				return;
 			}
 			System.out.println(Thread.currentThread().getName() + "当前正在销售的票号为" + num-- + ",还剩下" + num + "张票");
-			check();
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
+//			check();
+//		} catch (InterruptedException e) {
+//			throw new RuntimeException(e);
 		} finally {
 			reentrantLock.unlock();
 		}
