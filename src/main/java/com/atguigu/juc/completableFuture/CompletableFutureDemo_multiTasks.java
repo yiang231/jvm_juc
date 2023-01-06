d4916e2890b314152a6ac2da5d498f261683d5cf
Yiang <1712064922@qq.com>
2023-01-06 20:47:18 +0800

    其他测试

--- src/main/java/com/atguigu/juc/completableFuture/CompletableFutureDemo_multiTasks.java
+++ src/main/java/com/atguigu/juc/completableFuture/CompletableFutureDemo_multiTasks.java
@@ -2,13 +2,14 @@ package com.atguigu.juc.completableFuture;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 // 多任务组合
 public class CompletableFutureDemo_multiTasks {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		ThreadPoolExecutor executor = new ThreadPoolExecutor(121, 423, 2L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(4343), new ThreadFactory() {
 			int i = 1;
 
@@ -22,17 +23,19 @@ public class CompletableFutureDemo_multiTasks {
 		CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
 			System.out.println(Thread.currentThread().getName() + "\t任务1");
 		}, executor);
-
+		System.out.println(Thread.currentThread() + "\tfuture1 = " + future1);
 		CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
-			throw new ArithmeticException();
-//			System.out.println(Thread.currentThread().getName() + "\t任务2");
-//			return 123;
+//			throw new ArithmeticException();
+			System.out.println(Thread.currentThread().getName() + "\t任务2");
+			return 123;
 		}, executor);
-
+		// 这两条语句所有组合有不同结果
+		System.out.println(Thread.currentThread() + "\tfuture2 = " + future2);
+		System.out.println(Thread.currentThread() + "\tfuture2 = " + future2.get());
 		CompletableFuture<Void> future3 = CompletableFuture.runAsync(() -> {
 			System.out.println(Thread.currentThread().getName() + "\t任务3");
 		}, executor);
-
+		System.out.println(Thread.currentThread() + "\tfuture3 = " + future3);
 		//以上三个异步任务全部完成,做另外一件事,比如保存数据库等操作
 		CompletableFuture.allOf(future1, future2, future3)
 				.whenCompleteAsync((unused, throwable) -> {
@@ -42,6 +45,7 @@ public class CompletableFutureDemo_multiTasks {
 					System.out.println(Thread.currentThread().getName() + "\t" + unused + "\t保数据库");
 				}, executor);
 		executor.shutdown();
+		System.out.println(Thread.currentThread() + "\tmission complete");
 		TimeUnit.SECONDS.sleep(3);
 	}
 }
